7ac3287b5415d5b6e2c98a5c0c161e42b4649449
Mark Drago <markdrago@gmail.com>
2007-07-08 18:33:19 -0400

    get closer to a complete ui mockup that does absolutely nothing

--- src/ui_gtk.c
+++ src/ui_gtk.c
@@ -10,16 +10,18 @@
 /* gettext macro */
 #define _(string) gettext(string)
 
-static void setup_main_window(GtkContainer *window) {
-  GtkWidget *main_hbox;
+static void ui_gtk_get_name_container(GtkWidget **name_container)
+{
   GtkListStore *name_list_store;
   GtkTreeIter iter;
   GtkCellRenderer *renderer;
   GtkWidget *view;
-  GtkWidget *left_label;
   GtkTreeViewColumn *tree_column;
+  GtkWidget *adder_hbox;
+  GtkWidget *adder_entry;
+  GtkWidget *adder_button;
 
-  main_hbox = gtk_hbox_new(FALSE, 0);
+  /* create the list store & tree view */
   name_list_store = gtk_list_store_new(3, G_TYPE_INT, G_TYPE_STRING, G_TYPE_INT);
   view = gtk_tree_view_new();
 
@@ -29,6 +31,7 @@ static void setup_main_window(GtkContainer *window) {
   gtk_list_store_append(name_list_store, &iter);
   gtk_list_store_set(name_list_store, &iter, 0, 2, 1, "Byron", 2, 8, -1);
 
+  /* tell the tree view how to render the columns */
   renderer = gtk_cell_renderer_text_new();
   gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(view), -1, _("#"),
 					      renderer, "text", 0, NULL);
@@ -39,14 +42,55 @@ static void setup_main_window(GtkContainer *window) {
   gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(view), -1, _("Score"),
 					      renderer, "text", 2, NULL);
 
+  /* marry the view to the model */
   gtk_tree_view_set_model(GTK_TREE_VIEW(view), GTK_TREE_MODEL(name_list_store));
   g_object_unref(name_list_store);
 
-  left_label = gtk_label_new("Test");
+  /* create hbox for adder that is below the names */
+  adder_hbox = gtk_hbox_new(FALSE, 0);
+
+  /* create a text entry and button for adder */
+  adder_entry = gtk_entry_new();
+  adder_button = gtk_button_new_from_stock(GTK_STOCK_ADD);
+  gtk_widget_set_size_request(adder_entry, 100, -1);
+  
+  /* pack entry and button in to the hbox */
+  gtk_box_pack_start(GTK_BOX(adder_hbox), adder_entry, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(adder_hbox), adder_button, FALSE, FALSE, 0);
+
+  /* create vbox for names and adder */
+  *name_container = gtk_vbox_new(FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(*name_container), view, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(*name_container), adder_hbox, FALSE, FALSE, 0);
+}
+
+static void ui_gtk_get_control_container(GtkWidget **control_container) {
+  GtkWidget *current_player, *label2;
+
+  current_player = gtk_label_new(NULL);
+  gtk_label_set_markup(GTK_LABEL(current_player),
+		       "<span size=\"large\" weight=\"bold\">Mark</span>");
+  gtk_label_set_justify(GTK_LABEL(current_player), GTK_JUSTIFY_LEFT);
+  label2 = gtk_label_new("Label2");
+  *control_container = gtk_vbox_new(TRUE, 0);
+
+  gtk_box_pack_start(GTK_BOX(*control_container), current_player, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(*control_container), label2, TRUE, TRUE, 0);
+}
+
+static void ui_gtk_get_main_window(GtkContainer *window) {
+  GtkWidget *main_hbox;
+  GtkWidget *control_container;
+  GtkWidget *list_container;
+
+  main_hbox = gtk_hbox_new(FALSE, 0);
+
+  ui_gtk_get_name_container(&list_container);
+  ui_gtk_get_control_container(&control_container);
 
   /* pack widgets in to the hbox */
-  gtk_box_pack_start(GTK_BOX(main_hbox), left_label, TRUE, TRUE, 2);
-  gtk_box_pack_start(GTK_BOX(main_hbox), view, FALSE, FALSE, 2);
+  gtk_box_pack_start(GTK_BOX(main_hbox), control_container, TRUE, TRUE, 2);
+  gtk_box_pack_start(GTK_BOX(main_hbox), list_container, FALSE, FALSE, 2);
 
   gtk_container_add(GTK_CONTAINER(window), main_hbox);
 }
@@ -78,7 +122,7 @@ void ui_gtk_init(int argc, char *argv[]) {
   gtk_window_set_default_size(window, 400, 200);
 #endif
 
-  setup_main_window(GTK_CONTAINER(window));
+  ui_gtk_get_main_window(GTK_CONTAINER(window));
 
   /* make close work */
   g_signal_connect(G_OBJECT(window), "delete_event",
